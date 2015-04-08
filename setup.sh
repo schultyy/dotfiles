6fdcd22effe866c7273b62add241b69b606b3dee
Jan Schulte <jan.schulte@asquera.de>
2015-04-08 07:54:10 +0000

    Check if Vundle is already there

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ -d ~/.vim/bundle/Vundle.vim ] ; then
+  echo "Vundle already exists"
+else
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/ftplugin ~/.vim/ftplugin
