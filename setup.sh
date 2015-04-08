ba9921ff46f56a6c11981be6fe565a7378236533
Jan Schulte <jan.schulte@asquera.de>
2015-04-08 08:02:25 +0000

    Check if links are already present and preserve them

--- setup.sh
+++ setup.sh
@@ -4,7 +4,22 @@ if [ -d ~/.vim/bundle/Vundle.vim ] ; then
 else
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+
+if [ -f ~/.vimrc ] ; then
+  echo "Found existing .vimrc. Moving to ~/.vimrc.old"
+  mv ~/.vimrc ~/.vimrc.old
+fi
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+if [ -f ~/.zshrc ] ; then
+  echo "Found existing .zshrc. Moving to ~/.zshrc.old"
+  mv ~/.zshrc ~/.zshrc.old
+fi
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+if [ -d ~/.vim/ftplugin ] ; then
+  echo "Found existing ~/.vim/ftplugin. Moving to ~/.vim/ftplugin_old"
+  mv ~/.vim/ftplugin ~/.vim/ftplugin_old
+fi
 ln -s ~/dotfiles/ftplugin ~/.vim/ftplugin
 
