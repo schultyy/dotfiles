5da454439b165adaf8b53d5b40195d61abbc4999
Jan Schulte <janschulte@fastmail.com>
2022-12-16 10:13:33 -0500

    symlink vim config files

--- vim.sh
+++ vim.sh
@@ -8,11 +8,13 @@ rm -rf /Users/jan/.local/share/nvim/site/pack/packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-cp nvim/init.lua ~/.config/nvim/init.lua
-cp nvim/plugins.lua ~/.config/nvim/lua/plugins.lua
-cp nvim/colorschemes.lua ~/.config/nvim/lua/colorschemes.lua
-cp nvim/rust.lua ~/.config/nvim/lua/rust.lua
-cp nvim/opts.lua ~/.config/nvim/lua/opts.lua
-cp nvim/completions.lua ~/.config/nvim/lua/completions.lua
+ln -s $(pwd)/nvim ~/.config/
+
+# cp nvim/init.lua ~/.config/nvim/init.lua
+# cp nvim/plugins.lua ~/.config/nvim/lua/plugins.lua
+# cp nvim/colorschemes.lua ~/.config/nvim/lua/colorschemes.lua
+# cp nvim/rust.lua ~/.config/nvim/lua/rust.lua
+# cp nvim/opts.lua ~/.config/nvim/lua/opts.lua
+# cp nvim/completions.lua ~/.config/nvim/lua/completions.lua
 
 
