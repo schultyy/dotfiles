3951a357580c2ce3f8c897b51b6142877759df21
Jan Schulte <janschulte@fastmail.com>
2023-01-07 10:34:10 -0500

    Install ctrlp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -70,6 +70,8 @@ return require('packer').startup(function(use)
   end
   }
 
+  use 'ctrlpvim/ctrlp.vim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
